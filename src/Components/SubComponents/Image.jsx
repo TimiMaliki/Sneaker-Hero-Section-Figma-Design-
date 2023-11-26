be9d4931e96f217b7a5f28ee9e8c05135fe92efa
TimiMaliki <malikitimi@gmail.com>
2023-11-26 13:13:05 +0100

    added:the grid section

--- src/Components/SubComponents/Image.jsx
+++ src/Components/SubComponents/Image.jsx
@@ -6,7 +6,7 @@ const Image = () => {
   return (
     <div className="lg:-ml-0 -ml-44  lg:-mt-64  mt-44 relative">
       <h4 className="Image-layout-text">NYKEN AIR</h4>
-      <div className="absolute top-20">
+      <div className="absolute lg:top-20 top-[75%]">
         <img src={Nike} alt="" className="image"  style={{background:"lightgray -75.646px -253.085px / 121.311% 188.627% , no-repeat"}}/>
       </div>
     </div>
