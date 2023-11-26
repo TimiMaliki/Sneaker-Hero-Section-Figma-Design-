3a043e7028b50399a0096e31f66feaaf7ede131c
TimiMaliki <malikitimi@gmail.com>
2023-11-26 15:26:00 +0100

    added:swiper js

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,22 +1,41 @@
 import NavBar from "../NavBar/NavBar";
 import Grid from "../SubComponents/Grid";
-import Atropos from "atropos/react";
-import "atropos/css";
+// import Atropos from "atropos/react";
+// import "atropos/css";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+// import required modules
+import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper/modules';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 
 const Hero = () => {
   return (
     <div >
         {/* className="bg-[#F9F9F9]" */}
-      <NavBar />
-      {/* <Atropos
-        activeOffset={40}
-        shadowScale={0.05}
-        onEnter={() => console.log("Enter")}
-        onLeave={() => console.log("Leave")}
-        onRotate={(x, y) => console.log("Rotate", x, y)}
-      > */}
-        <Grid />
-      {/* </Atropos> */}
+        <NavBar />
+        <Swiper
+        cssMode={true}
+        navigation={true}
+        pagination={true}
+        mousewheel={true}
+        keyboard={true}
+        modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+        className="mySwiper"
+      >
+        <SwiperSlide><Grid /></SwiperSlide>
+        <SwiperSlide><Grid /></SwiperSlide>
+       
+      </Swiper>
+     
+    
+    
+ 
     </div>
   );
 };
